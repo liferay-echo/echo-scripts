de492485e2e775632353a7aeab765287ad38bdee
Rubén Pulido <ruben.pulido@liferay.com>
2023-04-26 09:05:31 +0200

    LPS-158078 Create wh-labels script, used for adding labels to a PR based on the extensions of the files changed

---
+++ wh-labels/wh-labels.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+whtype()
+{
+	echo Content-Type: text/plain;charset=utf-8
+	echo Status: $1
+	echo
+	echo $2
+}
+
+[ $REQUEST_METHOD != POST ] && whtype 405 "? post" && exit 1
+
+whhome=@HOME
+whconf=$whhome/.wh
+
+[ ! -f $whconf/whtoken ] && whtype 500 "? whtoken" && exit 1
+
+[ $HTTP_X_GITHUB_EVENT != pull_request ] && whtype 200 "i ev $HTTP_X_GITHUB_EVENT" && exit 0
+
+set -- `jq -r '[.action, .pull_request.comments_url, .pull_request.url + "/files", .pull_request.issue_url + "/labels", .pull_request.user.login]|@tsv'`
+
+action=$1
+comments_url=$2
+files_url=$3
+labels_url=$4
+user=$5
+
+[ "$action" != opened ] && whtype 200 "i ac $1" && exit 0
+
+if grep -q ^"$user"$ "$whconf/users"
+then
+  whtype 200 "skipped user $user";
+  exit 0;
+fi
+
+whtype 200 "Ok"
+
+token=`cat $whconf/whtoken`
+
+whlsext()
+{
+	curl -u $token -H "Accept: application/vnd.github.v3+json" $1 |
+	jq -r '.[].filename|@text' |
+	sed -e '/\./!d' -e 's/.*\.\([^.]*\)$/\1/' |
+	sort -u
+}
+
+whlabel()
+{
+	tmp=`mktemp -t wh`
+	cat >$tmp
+	grep -F -l -x -f $tmp $whconf/labels/* |
+	tr ' ' '_' |
+	xargs -n 1 basename |
+	tr '_' ' ' |
+	awk 'BEGIN { printf "{\"labels\":[" } { printf "\"%s\",", $0 } END { printf "]}" }' |
+	sed 's/,]}/]}/' |
+	curl -X POST -u $token $1 -d @- >/dev/null
+	rm $tmp
+}
+
+whlsext "$files_url" | whlabel "$labels_url"
